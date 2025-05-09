976ddbb9e78221b27769b58ada2933105823db4c
LindaTancini <lindatancini95@gmail.com>
2025-05-09 16:06:27 +0200

    feat: add query review and testing on postman

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -16,6 +16,8 @@ function show(req, res) {
   const { id } = req.params;
   //Query
   const sql = `SELECT * FROM movies WHERE id = ?`;
+  // Query Review
+  const sqlReview = `SELECT * FROM reviews WHERE movie_id = ?`;
   //Eseguo la query
   connection.query(sql, [id], (err, results) => {
     if (err) return res.status(500).json({ error: "La query al db è fallita" });
@@ -23,7 +25,13 @@ function show(req, res) {
       return res.status(404).json({ error: "Il post non è stato trovato" });
     //Invio la risposta
     const movie = results[0];
-    res.json(movie);
+    //Eseguo la query per la review
+    connection.query(sqlReview, [id], (err, resultsReview) => {
+      if (err)
+        return res.status(500).json({ error: "La query al db è fallita" });
+      movie.reviews = resultsReview;
+      res.json(movie);
+    });
   });
 }
 
