10118535f47f1db588183ba4b9cbf0a2618ffee4
LindaTancini <lindatancini95@gmail.com>
2025-05-09 14:37:20 +0200

    feat: add homepage and server port (testing on postman)

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Ciao Linda");
+//Express
+const express = require("express");
+const app = express();
+const port = 3000;
+//HomePage
+app.get("/", (req, res) => {
+  res.send("Homepage della Webapp!");
+});
+// Avvio server
+app.listen(port, () => {
+  console.log(`Sono un server attivo sulla porta:${port}`);
+});
